23c0eea115fabdc79da81d9a6b1eaffe6b7e38bb
KU-JonasL <138462052+KU-JonasL@users.noreply.github.com>
2023-12-20 20:28:45 +0100

    Add files via upload
    Fourth upload. Version 0.0.4. Change in on app route (Index routing)

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('template\htmls\index.html')
+    return render_template('index.html')
 
 @app.route('/OT2protocol')
 def get_finished_protocols():
