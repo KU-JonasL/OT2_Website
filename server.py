82e73e089f2b1d80ca3316752cae7bb57bb4ae4e
KU-JonasL <138462052+KU-JonasL@users.noreply.github.com>
2024-01-02 15:24:16 +0100

    Add files via upload
    Trial upload 0.0.1

--- server.py
+++ server.py
@@ -34,43 +34,6 @@ app.config["Client_Scripts"] = os.path.join(app.root_path, 'template', 'client',
 @app.route('/index', methods=['POST', 'GET'])
 def index():
 
-    print("At index")
-
-    ## Creating a client directory if not existing
-    directory = 'template/client/csv'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-
-    if request.method == "POST":
-        
-        ##Naming of new template(s) selected (based on)
-        protocolselect = request.form.get('protocol')[0]
-        user = request.form.get('user')[0]
-        today = datetime.today().strftime('%Y%m%d')
-        naming = user+"_"+protocolselect+"_"+today
-
-
-        if request.files:
-            
-            myFile = request.files['myFile']
-
-            filename = secure_filename(myFile.filename)
-
-            if myFile.filename == "":
-                return redirect(request.url)
-
-            else:
-                df = pd.read_csv(myFile)
-                df.to_csv(f'template/client/csv/{naming}_userinput.csv', index=False)
-                
-                #with open(f'template/client/csv/{naming}.csv','w') as modified_csv:
-                #    new_csv = os.path.join(f'template/client/csv/{naming}', filename)
-                #    modified_csv.write(new_csv)
-                            
-            
-            return redirect('/OT2transfer.html')
-
     return render_template('/index.html')
 
 
@@ -78,94 +41,26 @@ def index():
 @app.route('/OT2transfer', methods=['POST', 'GET'])
 def get_OT2transfer():
     
-    if request.method == "POST":
+    ## Arguments pasted in
+    protocol = request.form.get('protocol')[0]
+    user = request.form.get('user')[0]
+    samplenumber = request.form.get('samples')[0]
+    inputformat = request.form.get('inputformat')[0] 
+    outputformat = request.form.get('outputformat')[0]
+        
+    ## User Data redirect into a dataframe 
+    userinput = pd.DataFrame({'Protocol':[protocol],'User':[user],'SampleNumber':[samplenumber],'InputFormat':[inputformat],'OutputFormat':[outputformat]})
 
-        ## Creating a client directory if not existing
-        directory = 'template/client/csv'
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-                    
-        ## Arguments pasted in
-        protocol = request.form.get('protocol')[0]
-        user = request.form.get('user')[0]
-        samplenumber = request.form.get('samples')[0]
-        inputformat = request.form.get('inputformat')[0] 
-        outputformat = request.form.get('outputformat')[0]
+    return render_template(
+    "/OT2transfer.html",
+    protocol = userinput['Protocol'],
+    user = userinput['User'],
+    samplenumber = userinput['SampleNumber'],
+    inputformat = userinput['InputFormat'],
+    outputformat = userinput['OutputFormat'],
+    )
         
 
-        ## User Data redirect into a dataframe 
-        userinput = pd.DataFrame({'Protocol':[protocol],'User':[user],'SampleNumber':[samplenumber],'InputFormat':[inputformat],'OutputFormat':[outputformat]})
-
-        ##Naming of new template(s) selected (based on)
-        today = datetime.today().strftime('%Y%m%d')
-        naming = user+"_"+protocol+"_"+today
-
-        ## Exporting user inputs
-        userinput.to_csv(f'template/client/csv/{naming}_userinput.csv', index=False)
-        
-        #with open(f'template/client/csv/{naming}_userinput.csv','w') as modified_csv:
-        #    modified_csv.write(userinput)
-
-
-
-        ## Downloading csv data
-        file = get_csv(f'{naming}.csv')
-            
-
-        try:
-            ## Folder for user csv data files
-            print("Trying the transferred file")
-            
-            userdata = pd.read_csv(file)
-            
-
-            ## Incorprate user data and input into the csvOT2transfer function.
-            if file != '':
-                    get_opentrons_script(protocol,user,samplenumber,inputformat,outputformat,userdata)
-
-            elif file == '' and protocol != "Library":
-                    userdata = pd.read_csv(file)
-                    get_opentrons_script(protocol,user,samplenumber,inputformat,outputformat,userdata = 0 )
-
-            elif file == '' and protocol == "Library":
-                return render_template('/index.html')
-
-            finished_protocols = [0,0,0]
-
-            if protocol == "Extraction":
-                finished_protocols[0] = get_opentrons_script(f'{naming}_Extraction.py')
-
-            elif protocol == "Library":
-                finished_protocols[0] = get_opentrons_script(f'{naming}_covaris.py')
-                finished_protocols[1] = get_opentrons_script(f'{naming}_BESTLibrary.py')
-                finished_protocols[2] = get_opentrons_script(f'{naming}_BESTPurification.py')
-
-            elif protocol == "qPCR":
-                finished_protocols[0] = get_opentrons_script(f'{naming}_qPCR.py')
-
-            elif protocol == "IndexPCR":
-                finished_protocols[0] = get_opentrons_script(f'{naming}_IndexPCR.py')
-                finished_protocols[1] = get_opentrons_script(f'{naming}_IndexPurification.py')
-            
-            
-            
-            return render_template(
-                "/OT2transfer.html",
-                protocol = userinput['Protocol'],
-                user = userinput['User'],
-                samplenumber = userinput['SampleNumber'],
-                inputformat = userinput['InputFormat'],
-                outputformat = userinput['OutputFormat'],
-                finished_protocol1=finished_protocols[1],
-                finished_protocol2=finished_protocols[2],
-                finished_protocol3=finished_protocols[3])
-        
-        except FileNotFoundError:
-            print("Did not find a csv file")
-            abort(404)
-
-    else: 
-        return render_template("/index.html")
 
 @app.route("/get-csv/<path:name>")
 def get_csv(name):
