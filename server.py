2d5d697ad5a7e7683af0844a945762ed01645ffb
Jonas Lauritsen <jonas.lauritsen@sund.ku.dk>
2025-08-08 15:15:04 +0200

    Attempt to correct server.py to include SOP

--- server.py
+++ server.py
@@ -193,7 +193,7 @@ def get_opentrons_script(protocol, user, samplenumber, inputformat, outputformat
             ## Add SOP for extraction to zipfolder
             file_path = os.path.join(app.root_path,'static','SOPs', 'SOP_Template_V1.1.pdf')
             static_sop_content = open(file_path, 'r').read()
-            zipf.writestr('SOP_V1.1.pdf', static_sop_content)
+            zipf.writestr('SOP_EHI-OT2_AlberdiLab_V1.1.pdf', static_sop_content)
 
 
         
@@ -238,7 +238,7 @@ def get_opentrons_script(protocol, user, samplenumber, inputformat, outputformat
             ## Add SOP for extraction to zipfolder
             file_path = os.path.join(app.root_path,'static','SOPs', 'SOP_Template_V1.1.pdf')
             static_sop_content = open(file_path, 'r').read()
-            zipf.writestr('SOP_V1.1.pdf', static_sop_content)
+            zipf.writestr('SOP_EHI-OT2_AlberdiLab_V1.1.pdf', static_sop_content)
 
 
 
@@ -264,7 +264,7 @@ def get_opentrons_script(protocol, user, samplenumber, inputformat, outputformat
             ## Add SOP for extraction to zipfolder
             file_path = os.path.join(app.root_path,'static','SOPs', 'SOP_Template_V1.1.pdf')
             static_sop_content = open(file_path, 'r').read()
-            zipf.writestr('SOP_V1.1.pdf', static_sop_content)
+            zipf.writestr('SOP_EHI-OT2_AlberdiLab_V1.1.pdf', static_sop_content)
 
 
 
@@ -296,7 +296,7 @@ def get_opentrons_script(protocol, user, samplenumber, inputformat, outputformat
             ## Add SOP for extraction to zipfolder
             file_path = os.path.join(app.root_path,'static','SOPs', 'SOP_Template_V1.1.pdf')
             static_sop_content = open(file_path, 'r').read()
-            zipf.writestr('SOP_V1.1.pdf', static_sop_content)
+            zipf.writestr('SOP_EHI-OT2_AlberdiLab_V1.1.pdf', static_sop_content)
 
 
 
